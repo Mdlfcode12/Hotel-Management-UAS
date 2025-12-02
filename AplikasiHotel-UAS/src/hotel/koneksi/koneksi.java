e666ab4815fde06fd22001e0a144c195cd69f50d
Mdlfcode12 <akundeveloper417@gmail.com>
2025-12-02 16:33:45 +0700

    Update koneksi.java

--- AplikasiHotel-UAS/src/hotel/koneksi/koneksi.java
+++ AplikasiHotel-UAS/src/hotel/koneksi/koneksi.java
@@ -4,16 +4,8 @@
  * and open the template in the editor.
  */
 package hotel.koneksi;
-
-/**
- *
- * @author MyBook Z Series
- */
 public class koneksi {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
