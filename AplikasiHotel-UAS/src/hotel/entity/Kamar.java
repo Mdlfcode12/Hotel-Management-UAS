01620502b83f4c2a705e7bf38ff85b0287ca4d6b
hutamaarthur-sudo <hutamaarthur@gmail.com>
2025-12-03 13:27:42 +0700

    update kamar yoh

---
+++ AplikasiHotel-UAS/src/hotel/entity/Kamar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.entity;
+
+/**
+ *
+ * @author MyBook Z Series
+ */
+public class Kamar {
+    private String kodeKamar;
+	private String tipeKamar;
+	private int harga;
+	private String status;
+ 
+	public Kamar() {}
+ 
+	// Getter & Setter (Generate otomatis di NetBeans: Alt+Insert)
+	public String getKodeKamar() { return kodeKamar; }
+	public void setKodeKamar(String kodeKamar) { this.kodeKamar = kodeKamar; }
+ 
+	public String getTipeKamar() { return tipeKamar; }
+	public void setTipeKamar(String tipeKamar) { this.tipeKamar = tipeKamar; }
+ 
+	public int getHarga() { return harga; }
+	public void setHarga(int harga) { this.harga = harga; }
+ 
+	public String getStatus() { return status; }
+	public void setStatus(String status) { this.status = status; }
+
+}
